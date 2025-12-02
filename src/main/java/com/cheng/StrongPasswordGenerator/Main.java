e7d773da1cb10e20444068ca5458de27a9d83a27
Cheng31869 <3186977970@qq.com>
2025-12-02 15:25:58 +0800

    提升了在生成超长密码情况下的性能

--- src/main/java/com/cheng/StrongPasswordGenerator/Main.java
+++ src/main/java/com/cheng/StrongPasswordGenerator/Main.java
@@ -15,14 +15,14 @@ public class Main
         {
             System.out.println("请输入要生成多少位的密码");
             int length = sc.nextInt();
-            String password = "";
+            StringBuilder password = new StringBuilder("");
             for (int i = 0; i < length; i++)
             {
                 int tempChar = rand.nextInt(94) + 33;
-                password += (char) tempChar;
+                password.append((char) tempChar);
             }
             System.out.println("强密码生成完成");
-            System.out.println(password);
+            System.out.println(password.toString());
         }
 
     }
