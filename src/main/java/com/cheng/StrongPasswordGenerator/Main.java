cca0082e12618bbc2186cce7c9da9375b6a22e5e
Cheng31869 <3186977970@qq.com>
2025-12-02 14:14:16 +0800

    Initial commit

---
+++ src/main/java/com/cheng/StrongPasswordGenerator/Main.java
@@ -0,0 +1,29 @@
+package com.cheng.StrongPasswordGenerator;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        rand.setSeed(System.currentTimeMillis());
+
+        while (true)
+        {
+            System.out.println("请输入要生成多少位的密码");
+            int length = sc.nextInt();
+            String password = "";
+            for (int i = 0; i < length; i++)
+            {
+                int tempChar = rand.nextInt(94) + 33;
+                password += (char) tempChar;
+            }
+            System.out.println("强密码生成完成");
+            System.out.println(password);
+        }
+
+    }
+}
